8df4187a0a4025f054c1641d3060a65f9f3e1c0f
Anthony Bonta <zthun.coding@gmail.com>
2023-02-23 14:07:12 -0600

    refactor: rename to helpful-fn

--- packages/funcii-dash/src/index.ts
+++
@@ -1,4 +0,0 @@
-import * as _ from 'lodash';
-import * as _joinList from './join-list/join-list';
-
-export = Object.assign({}, _, _joinList);
---
+++ packages/helpful-fn/src/index.ts
@@ -0,0 +1,3 @@
+export * from './first-defined/first-defined';
+export * from './join-list/join-list';
+export * from './sleep/sleep';
