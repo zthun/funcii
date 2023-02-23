ac7d46ec83300c83b6511291896b7f2780d2139d
Anthony Bonta <zthun.coding@gmail.com>
2023-02-23 17:57:22 -0600

    feat: logic filters allow for and/or clauses

---
+++ packages/helpful-query/src/filter/logic-filter.spec.ts
@@ -0,0 +1,43 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import { ZBinaryFilterBuilder } from './binary-filter';
+import { ZCollectionFilterBuilder } from './collection-filter';
+import { IZFilter } from './filter';
+import { ZLogicFilterBuilder, ZLogicOperator } from './logic-filter';
+import { ZUnaryFilterBuilder } from './unary-filter';
+
+describe('LogicFilterBuilder', () => {
+  let clauseA: IZFilter;
+  let clauseB: IZFilter;
+  let clauseC: IZFilter;
+  let clauseD: IZFilter;
+
+  function createTestTarget() {
+    return new ZLogicFilterBuilder();
+  }
+
+  beforeEach(() => {
+    clauseA = new ZBinaryFilterBuilder().subject('age').greaterThan().value(2).build();
+    clauseB = new ZBinaryFilterBuilder().subject('age').lessThan().value(10).build();
+    clauseC = new ZUnaryFilterBuilder().field('collection').isNull().build();
+    clauseD = new ZCollectionFilterBuilder().subject('state').in().value('Texas').value('Arizona').build();
+  });
+
+  it('sets the clauses.', () => {
+    const expected = [clauseA, clauseB, clauseC, clauseD];
+    expect(createTestTarget().clauses(expected).build().clauses).toEqual(expected);
+  });
+
+  it('adds clauses.', () => {
+    const expected = [clauseA, clauseB, clauseC, clauseD];
+    const actual = createTestTarget().clause(clauseA).clause(clauseB).clause(clauseC).clause(clauseD).build().clauses;
+    expect(actual).toEqual(expected);
+  });
+
+  it('sets the operator to and.', () => {
+    expect(createTestTarget().and().clause(clauseA).clause(clauseB).build().operator).toEqual(ZLogicOperator.And);
+  });
+
+  it('sets the operator to or.', () => {
+    expect(createTestTarget().or().clause(clauseA).clause(clauseB).build().operator).toEqual(ZLogicOperator.Or);
+  });
+});
