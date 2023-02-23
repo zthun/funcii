ca28b70ba8ad08b9e406f5f8523b5ed4fd2db6da
Anthony Bonta <zthun.coding@gmail.com>
2023-02-23 14:09:50 -0600

    feat: sleep is a basic set timeout that waits for a specific set of milliseconds

---
+++ packages/helpful-fn/src/sleep/sleep.spec.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from 'vitest';
+import { sleep } from './sleep';
+
+describe('Sleep', () => {
+  it('should resolve almost immediately.', async () => {
+    await expect(sleep()).resolves.toBeUndefined();
+  });
+
+  it('should resolve after some time.', async () => {
+    await expect(sleep(10)).resolves.toBeUndefined();
+  });
+});
