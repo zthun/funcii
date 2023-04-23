b162132ac975a5fd20b9adb982054d5acea5efa5
Anthony Bonta <zthun.coding@gmail.com>
2023-04-23 10:17:56 -0500

    feat: added an orientation enum

---
+++ packages/helpful-fn/src/orientation/orientation.ts
@@ -0,0 +1,20 @@
+/**
+ * Represents a direction orientation.
+ */
+export enum ZOrientation {
+  /**
+   * Row orientation.
+   *
+   * This type of orientation is a row
+   * style orientation or flex-row orientation.
+   */
+  Horizontal = 'horizontal',
+
+  /**
+   * Vertical orientation.
+   *
+   * This type of orientation is a column
+   * style orientation or block orientation.
+   */
+  Vertical = 'vertical'
+}
