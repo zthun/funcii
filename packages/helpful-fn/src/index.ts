3df0fd0e7bf405a0e865a541391ef8778f8bffaf
Anthony Bonta <zthun.coding@gmail.com>
2023-04-23 11:21:07 -0500

    fix: export orientation and anchor

--- packages/helpful-fn/src/index.ts
+++ packages/helpful-fn/src/index.ts
@@ -1,5 +1,7 @@
+export * from './anchor/anchor';
 export * from './create-guid/create-guid';
 export * from './first-defined/first-defined';
 export * from './join-defined/join-defined';
+export * from './orientation/orientation';
 export * from './set-first/set-first';
 export * from './sleep/sleep';
