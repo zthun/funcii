a1cfac4a8c28ebf61aca4c91ee5d7dc285b304b0
Anthony Bonta <zthun.coding@gmail.com>
2023-06-10 18:48:53 -0500

    feat: well known classes give icon classes for common features

---
+++ packages/helpful-query/src/metadata/well-known-icon-classes.ts
@@ -0,0 +1,17 @@
+/**
+ * Represents well known icon font classes for reuse.
+ */
+export abstract class ZWellKnownIconClasses {
+  /**
+   * Font awesome.
+   *
+   * See https://fontawesome.com
+   */
+  public static readonly FontAwesome = 'https://fontawesome.com';
+  /**
+   * Material design.
+   *
+   * See https://mui.com/material-ui/material-icons.
+   */
+  public static readonly Material = 'https://mui.com/material-ui/material-icons';
+}
