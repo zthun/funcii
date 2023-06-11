493136df6b3fb27beb6d37a08e979a816cd3aff9
Anthony Bonta <zthun.coding@gmail.com>
2023-06-11 18:10:33 -0500

    feat: people data source now supports options

--- packages/helpful-people/src/people/data-source-people.spec.ts
+++ packages/helpful-people/src/people/data-source-people.spec.ts
@@ -1,11 +1,13 @@
-import { ZDataRequestBuilder } from '@zthun/helpful-query';
+import { IZDataSourceStaticOptions, ZDataRequestBuilder, ZDataSourceStaticOptionsBuilder } from '@zthun/helpful-query';
 import { describe, expect, it } from 'vitest';
+import { IZPerson } from '../person/person';
 import { ZDataSourcePeople } from './data-source-people';
 
 describe('Data Source People', () => {
   let count: number | undefined;
+  let options: IZDataSourceStaticOptions<IZPerson> | undefined;
 
-  const createTestTarget = () => new ZDataSourcePeople(count);
+  const createTestTarget = () => new ZDataSourcePeople(count, options);
 
   it('should return a page of people', async () => {
     // Arrange.
@@ -20,6 +22,7 @@ describe('Data Source People', () => {
   it('should return the expected number of people', async () => {
     // Arrange.
     count = 2000;
+    options = new ZDataSourceStaticOptionsBuilder().delay(0).build();
     const request = new ZDataRequestBuilder().size(100).build();
     const target = createTestTarget();
     // Act.
--- packages/helpful-people/src/people/data-source-people.ts
+++ packages/helpful-people/src/people/data-source-people.ts
@@ -1,6 +1,23 @@
-import { IZDataRequest, IZDataSource, ZDataSourceStatic } from '@zthun/helpful-query';
+import { sleep } from '@zthun/helpful-fn';
+import {
+  IZDataRequest,
+  IZDataSource,
+  IZDataSourceStaticOptions,
+  ZDataFilterFields,
+  ZDataSearchFields,
+  ZDataSourceStatic,
+  ZDataSourceStaticOptionsBuilder
+} from '@zthun/helpful-query';
 import { range } from 'lodash';
 import { IZPerson, ZPersonBuilder } from '../person/person';
+
+const DefaultPeopleStaticOptions = Object.freeze(
+  new ZDataSourceStaticOptionsBuilder<IZPerson>()
+    .search(new ZDataSearchFields())
+    .filter(new ZDataFilterFields())
+    .delay(200)
+    .build()
+) satisfies IZDataSourceStaticOptions<IZPerson>;
 
 /**
  * Represents a data source of randomly generated people.
@@ -15,14 +32,16 @@ export class ZDataSourcePeople implements IZDataSource<IZPerson> {
    * @param count -
    *        The total number of people to generate.
    */
-  public constructor(count = 5000) {
+  public constructor(count = 5000, options: IZDataSourceStaticOptions<IZPerson> = DefaultPeopleStaticOptions) {
     this._count = count;
 
+    // Note that the first call to count and retrieve have a double debounce.  This is intentional.
     this._people =
       this._people ||
       Promise.resolve(true)
+        .then(() => sleep(options?.delay))
         .then(() => range(0, this._count).map(() => new ZPersonBuilder().random().build()))
-        .then((p) => new ZDataSourceStatic(p));
+        .then((p) => new ZDataSourceStatic(p, options));
   }
 
   public async count(request: IZDataRequest): Promise<number> {
