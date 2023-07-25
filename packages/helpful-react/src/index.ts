fe30e5a868e9b0911474e0cb78c4b03235cbf987
Anthony Bonta <zthun.coding@gmail.com>
2023-07-25 17:57:47 -0500

    chore: export hook

--- packages/helpful-react/src/index.ts
+++ packages/helpful-react/src/index.ts
@@ -2,3 +2,5 @@ export * from './ambassador-state/use-ambassador-state';
 export * from './async-state/use-async-state';
 export * from './keyboard-activate/use-keyboard-activate';
 export * from './state-as-array/use-state-as-array';
+export * from './view-state/use-more-view-state';
+export * from './view-state/use-page-view-state';
