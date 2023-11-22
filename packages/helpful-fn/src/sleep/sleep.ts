4fa1d97661e9fb94fd911e724b03177e44dae758
Anthony Bonta <zthun.coding@gmail.com>
2023-11-22 16:22:57 -0600

    docs: return value

--- packages/helpful-fn/src/sleep/sleep.ts
+++ packages/helpful-fn/src/sleep/sleep.ts
@@ -16,9 +16,11 @@ export function sleep(ms?: number): Promise<void>;
  *        The total number of milliseconds to sleep.
  * @param val -
  *        The value to resolve with.
+ * @param T -
+ *        The type of value that will be resolved.
  *
  * @returns
- *        A promise that resolves after ms milliseconds.
+ *        A promise that resolves with val after ms milliseconds.
  */
 export function sleep<T>(ms: number, val: T): Promise<T>;
 
@@ -27,9 +29,11 @@ export function sleep<T>(ms: number, val: T): Promise<T>;
  *
  * @param ms -
  *        The total number of milliseconds to sleep.
+ * @param T -
+ *        The type of value that will be resolved.
  *
  * @returns
- *        A promise that resolves after ms milliseconds.
+ *        A promise that resolves with val after ms milliseconds.
  */
 export function sleep<T>(ms = 0, val: T | undefined = undefined): Promise<T | undefined> {
   return new Promise<T | undefined>((resolve) => setTimeout(() => resolve(val), ms));
