eef74c00252307641b816789357f124c8ff9bc11
Anthony Bonta <zthun.coding@gmail.com>
2023-11-22 16:21:03 -0600

    docs: examples and references

--- packages/helpful-fn/src/join-defined/join-defined.ts
+++ packages/helpful-fn/src/join-defined/join-defined.ts
@@ -1,15 +1,36 @@
+/**
+ * A set of parameters that can be used for a string join.
+ *
+ * Join lists can be a regular object, null, undefined, or a tuple where the
+ * first item is the object to join, and the second item is a boolean that specifies
+ * whether to include it if the value is truthy.
+ *
+ * @param T -
+ *        The type of data to join.
+ */
 export type JoinListInputParameter<T> = T | [T, boolean] | null | undefined;
 
 /**
- * Similar to a string join, but filters out null and undefined items.
+ * Similar to {@link Array.join}, but filters out null and undefined items.
  *
  * @param delimiter -
  *        The delimiter that separates the items.
  * @param items -
  *        The items to join.
+ * @param T -
+ *        The type of data to join.
  *
  * @returns
  *        A string that joins the items that are defined, separated by delimiter.
+ *
+ * @example
+ *
+ * ```ts
+ * // 'a b d'
+ * const joinBySpace = joinDefined(' ', 'a', 'b', ['c', false], null, undefined, ['d', true]);
+ * // (Empty String)
+ * const joinEmpty = joinDefined('?');
+ * ```
  */
 export function joinDefined<T>(delimiter: string, ...items: JoinListInputParameter<T>[]) {
   return items
@@ -19,13 +40,18 @@ export function joinDefined<T>(delimiter: string, ...items: JoinListInputParamet
 }
 
 /**
- * Alias to joinList with a string delimiter.
+ * Alias to joinList with a space delimiter.
  *
  * @param items -
  *        The items to join.
+ * @param T -
+ *        The type of data to join.
  *
  * @returns
  *        A string that joins the items that are defined, separated by space delimiter.
+ *
+ * @see
+ *        {@link joinDefined} for more information and examples.
  */
 export const spaceJoinDefined: <T>(...items: JoinListInputParameter<T>[]) => string = joinDefined.bind(null, ' ');
 
@@ -34,9 +60,14 @@ export const spaceJoinDefined: <T>(...items: JoinListInputParameter<T>[]) => str
  *
  * @param items -
  *        The items to join.
+ * @param T -
+ *        The type of data to join.
  *
  * @returns
  *        A string that joins the items that are defined, separated by a space delimiter.
+ *
+ * @see
+ *        {@link joinDefined} for more information and examples.
  */
 export const cssJoinDefined: <T>(...items: JoinListInputParameter<T>[]) => string = spaceJoinDefined;
 
@@ -45,9 +76,14 @@ export const cssJoinDefined: <T>(...items: JoinListInputParameter<T>[]) => strin
  *
  * @param items -
  *        The items to join.
+ * @param T -
+ *        The type of data to join.
  *
  * @returns
  *        A string that joins the items that are defined, separated by a comma delimiter.
+ *
+ * @see
+ *        {@link joinDefined} for more information and examples.
  */
 export const commaJoinDefined: <T>(...items: JoinListInputParameter<T>[]) => string = joinDefined.bind(null, ',');
 
@@ -56,9 +92,14 @@ export const commaJoinDefined: <T>(...items: JoinListInputParameter<T>[]) => str
  *
  * @param items -
  *        The items to join.
+ * @param T -
+ *        The type of data to join.
  *
  * @returns
  *        A string that joins the items that are defined, separated by a semi-colon delimiter.
+ *
+ * @see
+ *        {@link joinDefined} for more information and examples.
  */
 export const semiColonJoinDefined: <T>(...items: JoinListInputParameter<T>[]) => string = joinDefined.bind(null, ';');
 
@@ -67,8 +108,13 @@ export const semiColonJoinDefined: <T>(...items: JoinListInputParameter<T>[]) =>
  *
  * @param items -
  *        The items to join.
+ * @param T -
+ *        The type of data to join.
  *
  * @returns
  *        A string that joins the items that are defined, separated by a pipe delimiter.
+ *
+ * @see
+ *        {@link joinDefined} for more information and examples.
  */
 export const pipeJoinDefined: <T>(...items: JoinListInputParameter<T>[]) => string = joinDefined.bind(null, '|');
