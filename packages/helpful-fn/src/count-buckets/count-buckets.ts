7d7295c1f608cb613202fe7775c1518a5596bc8d
Anthony Bonta <zthun.coding@gmail.com>
2023-11-22 13:50:34 -0600

    docs: adding additional information and examples

--- packages/helpful-fn/src/count-buckets/count-buckets.ts
+++ packages/helpful-fn/src/count-buckets/count-buckets.ts
@@ -10,16 +10,25 @@
  * count.
  *
  * @param weight -
- *        The maximum weight a bucket can store.
+ *        The maximum weight a bucket can store.  If this value receives
+ *        Infinity, then it will result in 1 or min buckets being returned,
+ *        whichever is larger.  If this receives NaN, then this method will
+ *        result in NaN.  Finally, if this receives a negative value, then
+ *        the result will be max.
  * @param items -
  *        The total number of items you need to store where each item
- *        counts as 1 towards the weight.
+ *        counts as 1 towards the weight.  If the total number of items
+ *        is Infinity, then this method will result in max buckets.
+ *        If this receives NaN, then this method will result in NaN. Finally,
+ *        if you pass 0, or a negative number of items, then result will be min.
  * @param min -
  *        The bounded minimum value.  If the total number of buckets
  *        evaluates to less than this value, then this value is returned.
+ *        The default is 0.
  * @param max -
  *        The bounded maximum value.  If the total number of buckets
  *        evaluates to more than this value, then this value is returned.
+ *        The default is Infinity.
  *
  * @returns
  *        The number of buckets you need to store the total number
@@ -27,6 +36,23 @@
  *        If either weight or items is NaN, then NaN will be returned regardless
  *        of the opposite value.  Passing a negative number is the same as
  *        passing 0.
+ *
+ * @example
+ *
+ * ```ts
+ * // The following will return 5 for numberOfPages since you need 5 buckets
+ * // to store 101 number of items where each bucket can hold a max of 25
+ * // items.  The 5th page would be a page of 1 item, since 1 is the remainder.
+ * const numberOfPages = countBuckets(25, 101);
+ *
+ * // In this case, the numberOfPages would be 1 here since our minimum buckets
+ * // is 1.  By default, this would be 0.
+ * const numberOfPages = countBuckets(10, 0, 1);
+ *
+ * // In this case, we have more items that can be held in the number of buckets
+ * // available, so only 4 is returned instead of the 5.
+ * const numberOfPages = countBuckets(25, 101, undefined, 4);
+ * ```
  */
 export function countBuckets(weight: number, items: number, min = 0, max = Infinity) {
   weight = Math.max(0, weight);
