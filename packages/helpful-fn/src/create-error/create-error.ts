c12023b3e6ce5500035bb6a7e7116a8afd820825
Anthony Bonta <zthun.coding@gmail.com>
2023-11-22 15:52:49 -0600

    docs: added examples

--- packages/helpful-fn/src/create-error/create-error.ts
+++ packages/helpful-fn/src/create-error/create-error.ts
@@ -9,10 +9,24 @@
  *        The list of acceptable object keys to look into problem.  These will be recursively
  *        evaluated to strip out the real error message.  Note that this is in order of
  *        priority.  If schema[0] and schema[1] are both keys on problem, then schema[0] takes
- *        a higher precedence than schema[1].
+ *        a higher precedence than schema[1].  By default, the schema will look for properties
+ *        named, message, error, exception, and data, in that order.
  *
  * @returns
  *        An error object that is the best evaluation of what the problem actually is.
+ *
+ * @example
+ *
+ * ```ts
+ * // All of these result an Error with the message, 'Something went wrong'
+ * const errorWithStringProblem = createError('Something went wrong');
+ * const errorWithObjectProblemInSchema = createError({ error: 'Something went wrong'});
+ * const errorWithCustomSchema = createError({ issue: 'Something went wrong'}, ['issue']);
+ * const errorRecursive = createError({ error: { message: 'Something went wrong' }});
+ *
+ * // This would result in '[Object object]' as there is no way to figure out how to deconstruct this problem.
+ * const errorCannotBeFound = createError({ wut: 'Something went wrong' });
+ * ```
  */
 export function createError(problem: any, schema = ['message', 'error', 'exception', 'data']): Error {
   if (problem instanceof Error) {
