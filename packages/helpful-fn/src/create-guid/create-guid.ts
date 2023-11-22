b613408e97efbd7a8b14f87798a0dd7cfbd5d272
Anthony Bonta <zthun.coding@gmail.com>
2023-11-22 15:58:57 -0600

    docs: added explanations and the original spec

--- packages/helpful-fn/src/create-guid/create-guid.ts
+++ packages/helpful-fn/src/create-guid/create-guid.ts
@@ -3,7 +3,14 @@ import { v4 } from 'uuid';
 /**
  * Creates a globally unique identifier.
  *
+ * The identifier holds to v4 of the UUID specification.  A summary
+ * of the specification can be found at
+ * {@link https://commons.apache.org/sandbox/commons-id/uuid.html | Apache Commons UUID Documentation}.
+ *
+ * The official documentation of the UUID specification is under
+ * {@link https://www.rfc-editor.org/info/rfc4122 | RFC 4122}
+ *
  * @returns
- *        A new generated globally unique identifier.
+ *        A new generated globally unique identifier based on random bytes.
  */
 export const createGuid: () => string = v4;
