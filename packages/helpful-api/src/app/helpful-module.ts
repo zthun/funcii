22afcf25d4b58cf9fd7b33d28d41d7f5e06f21ac
Anthony Bonta <zthun.coding@gmail.com>
2023-11-22 16:33:00 -0600

    refactor!: remove helpful-api

--- packages/helpful-api/src/app/helpful-module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ZGuidsModule } from '../guid/guids-module';
-import { ZHealthModule } from '../health/health.module';
-
-@Module({
-  imports: [ZGuidsModule, ZHealthModule]
-})
-export class ZHelpfulModule {}
