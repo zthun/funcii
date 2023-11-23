25cabca91202dd11231fc7700601bb9477ab39f2
Anthony Bonta <zthun.coding@gmail.com>
2023-11-22 19:51:37 -0600

    docs: examples

--- packages/helpful-node/src/walk/walk.ts
+++ packages/helpful-node/src/walk/walk.ts
@@ -11,6 +11,8 @@ export interface ZWalkOptions {
    * The starting directory.
    *
    * The default is the current working directory
+   *
+   * @see {@link process.cwd} for more information.
    */
   start?: string;
 
@@ -31,10 +33,12 @@ export interface ZWalkOptions {
 }
 
 /**
- * Walks up a directory tree for a given path.
+ * Walks up a directory tree to search for existence of a given path.
  *
  * @param search -
- *        The path to search for.
+ *        The path to search for.  This is the candidate folder path that
+ *        may or may not exist somewhere from the start directory all the
+ *        way up the directory tree.
  * @param options -
  *        The given options for the search.
  *
@@ -42,6 +46,25 @@ export interface ZWalkOptions {
  *        The fully qualified path to the first path that matches the search
  *        starting at the options start or the current working directory if not
  *        specified.  Returns null if no such directory exists.
+ *
+ * @example
+ *
+ * ```ts
+ * import { resolve } from 'path'
+ *
+ * // Find any folder named .vscode from the current working directory all the way
+ * // up the directory tree.  Returns null if no vscode directory can be found.
+ * const vscode = walk('.vscode');
+ *
+ * // Walk the directory tree from the current script and find the .config folder.
+ * const config = walk('.config', { start: __dirname });
+ *
+ * // Walk the directory tree from the current script and stop when we reach
+ * // at most 2 directories up.
+ * const start = __dirname;
+ * const stop = resolve(__dirname, '../..');
+ * const file = walk('path/to/file.json', { start, stop });
+ * ```
  */
 export async function walk(search: string, options?: ZWalkOptions): Promise<string | null> {
   const start = options?.start || cwd();
