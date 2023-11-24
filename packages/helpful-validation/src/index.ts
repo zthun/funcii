a2f6fc108919c8000e9160433943bb506330eee4
Anthony Bonta <zthun.coding@gmail.com>
2023-11-24 08:40:49 -0600

    feat!: removed isDataUriType
    This is now implemented in version 0.14 of
    class-validator

--- packages/helpful-validation/src/index.ts
+++ packages/helpful-validation/src/index.ts
@@ -1,5 +1,4 @@
 export * from './data-uri/is-data-uri-limit';
-export * from './data-uri/is-data-uri-type';
 export * from './other-property/equals-other-property';
 export * from './other-property/requires-other-property';
 export * from './white-space/is-not-white-space';
