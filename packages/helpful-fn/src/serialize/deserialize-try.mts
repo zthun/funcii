2e2b2d04e7ff160e56b1aa06fe9d71c654d87685
Anthony Bonta <zthun.coding@gmail.com>
2024-02-09 14:50:23 -0600

    feat: deserialize-try allows for composite deserialization

---
+++ packages/helpful-fn/src/serialize/deserialize-try.mts
@@ -0,0 +1,39 @@
+import { IZDeserialize } from './deserialize.mjs';
+
+/**
+ * A deserializer that attempts to deserialize multiple times through a series of supported languages.
+ *
+ * @param T -
+ *        The type of data to deserialize.
+ * @param S -
+ *        The input type
+ */
+export class ZDeserializeTry<T, S = string> implements IZDeserialize<T, S> {
+  /**
+   * Initializes a new instance of this object.
+   *
+   * @param _children -
+   *        The list of deserializer objects to try on a specific candidate.
+   *        The first deserializer to succeed will return the target object.
+   *        If no deserializer is able to deserialize the candidate, then an
+   *        error is throw with a mapping of serialization errors between each
+   *        deserializer.
+   */
+  public constructor(private _children: IZDeserialize<T, S>[]) {}
+
+  public deserialize(candidate: S): T {
+    const errors: string[] = [];
+
+    for (const child of this._children) {
+      try {
+        return child.deserialize(candidate);
+      } catch (e) {
+        errors.push(e.message);
+      }
+    }
+
+    const msg = `Unable to deserialize candidate, ${candidate}.`;
+    errors.splice(0, 0, msg);
+    throw new Error(errors.join('\n\n'));
+  }
+}
---
+++ packages/helpful-fn/src/serialize/deserialize-try.spec.ts
@@ -0,0 +1,53 @@
+import { Mocked, beforeEach, describe, expect, it } from 'vitest';
+import { mock } from 'vitest-mock-extended';
+import { ZDeserializeTry } from './deserialize-try.mjs';
+import { IZDeserialize } from './deserialize.mjs';
+
+describe('ZDeserializeTry', () => {
+  let alpha: Mocked<IZDeserialize<any>>;
+  let beta: Mocked<IZDeserialize<any>>;
+  let charlie: Mocked<IZDeserialize<any>>;
+  let delta: Mocked<IZDeserialize<any>>;
+
+  const createTestTarget = () => new ZDeserializeTry([alpha, beta, charlie, delta]);
+
+  beforeEach(() => {
+    alpha = mock();
+    alpha.deserialize.mockImplementation(() => {
+      throw new Error('Cannot deserialize from alpha');
+    });
+    beta = mock();
+    beta.deserialize.mockImplementation(() => {
+      throw new Error('Cannot deserialize from beta');
+    });
+    charlie = mock();
+    charlie.deserialize.mockImplementation(() => {
+      throw new Error('Cannot deserialize from charlie');
+    });
+    delta = mock();
+    delta.deserialize.mockImplementation(() => {
+      throw new Error('Cannot deserialize from delta');
+    });
+  });
+
+  it('should return the first value deserialized properly', () => {
+    // Arrange.
+    const expected = 'From Charlie';
+    charlie.deserialize.mockReturnValue(expected);
+    delta.deserialize.mockReturnValue('From Delta');
+    const target = createTestTarget();
+    // Act.
+    const actual = target.deserialize('whatever');
+    // Assert.
+    expect(actual).toEqual(expected);
+  });
+
+  it('should throw an error if no deserializer can deserialize the value', () => {
+    // Arrange.
+    const target = createTestTarget();
+    // Act.
+    const actual = () => target.deserialize('whatever');
+    // Assert.
+    expect(actual).toThrowError();
+  });
+});
