5fb775c819611a71f15b90dd17b21460b7a837a8
Anthony Bonta <zthun.coding@gmail.com>
2024-02-09 14:49:36 -0600

    feat: deserialize-json enables schema based json deserialization

---
+++ packages/helpful-fn/src/serialize/deserialize-json.mts
@@ -0,0 +1,18 @@
+import { IZDeserialize } from './deserialize.mjs';
+
+/**
+ * A deserializer that deserializes a JSON string.
+ */
+export class ZDeserializeJson<T> implements IZDeserialize<T> {
+  public constructor(private _schema?: (k: any) => k is T) {}
+
+  public deserialize(candidate: string): T {
+    const parsed = JSON.parse(candidate);
+
+    if (this._schema && !this._schema(parsed)) {
+      throw new Error('The parsed JSON does not conform to the given schema requirement');
+    }
+
+    return parsed;
+  }
+}
---
+++ packages/helpful-fn/src/serialize/deserialize-json.spec.ts
@@ -0,0 +1,46 @@
+import { describe, expect, it } from 'vitest';
+import { ZDeserializeJson } from './deserialize-json.mjs';
+
+describe('ZDeserializeJson', () => {
+  function isString(k: any): k is string {
+    return typeof k === 'string';
+  }
+
+  const createTestTarget = (schema?: (k: any) => k is string) => new ZDeserializeJson<string>(schema);
+
+  it('should return the given object', () => {
+    // Arrange.
+    const target = createTestTarget();
+    const expected = 'Should be parsed from JSON';
+    // Act.
+    const actual = target.deserialize(JSON.stringify(expected));
+    // Assert.
+    expect(actual).toEqual(expected);
+  });
+
+  it('should return the given object, even if a type mismatch happens without a schema', () => {
+    // Arrange.
+    const target = createTestTarget();
+    const expected = 4;
+    // Act.
+    const actual = target.deserialize(JSON.stringify(expected));
+    // Assert.
+    expect(actual).toEqual(expected);
+  });
+
+  it('should throw an Error if the JSON cannot be parsed', () => {
+    // Arrange.
+    const target = createTestTarget();
+    // Act.
+    // Assert.
+    expect(() => target.deserialize('{ not valid json')).throws(Error);
+  });
+
+  it('should throw an Error if the JSON does not conform to the given schema', () => {
+    // Arrange.
+    const target = createTestTarget(isString);
+    // Act.
+    // Assert.
+    expect(() => target.deserialize(JSON.stringify(4))).throws(Error);
+  });
+});
