3674d8f02d4cc861857da2f54b6c7106e1e8e17d
Anthony Bonta <zthun.coding@gmail.com>
2024-02-15 12:35:13 -0600

    feat: $global returns the globalThis object
    This is the window for browsers and global for node

---
+++ packages/helpful-fn/src/global/global.mts
@@ -0,0 +1,2 @@
+/* istanbul ignore next -- @preserve */
+export const $global = typeof window === 'undefined' ? global : window;
--- packages/helpful-fn/src/index.ts
+++ packages/helpful-fn/src/index.ts
@@ -4,6 +4,7 @@ export * from './count-buckets/count-buckets.mjs';
 export * from './create-error/create-error.mjs';
 export * from './create-guid/create-guid.mjs';
 export * from './first-defined/first-defined.mjs';
+export * from './global/global.mjs';
 export * from './join-defined/join-defined.mjs';
 export * from './obligation/obligation.mjs';
 export * from './orientation/orientation.mjs';
