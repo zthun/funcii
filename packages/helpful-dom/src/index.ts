c9dafdaba984d4254490d7c7c009f1f8753fb51a
Anthony Bonta <zthun.coding@gmail.com>
2024-02-19 11:11:24 -0600

    fix: new functions are now exported

--- packages/helpful-dom/src/index.ts
+++ packages/helpful-dom/src/index.ts
@@ -1,1 +1,4 @@
+export * from './css-variable/css-variable.mjs';
+export * from './mutate-attribute/mutate-attribute.mjs';
+export * from './query-attribute/query-attribute.mjs';
 export * from './register-custom-element/register-custom-element.mjs';
