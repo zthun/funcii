89dd273f71f2b9423420be98704e15d40e5779ca
Anthony Bonta <zthun.coding@gmail.com>
2024-02-20 16:58:00 -0600

    chore: export new features

--- packages/helpful-dom/src/index.ts
+++ packages/helpful-dom/src/index.ts
@@ -2,3 +2,5 @@ export * from './css-variable/css-variable.mjs';
 export * from './mutate-attribute/mutate-attribute.mjs';
 export * from './query-attribute/query-attribute.mjs';
 export * from './register-custom-element/register-custom-element.mjs';
+export * from './web-component/component-lifecycle.mjs';
+export * from './web-component/component-property.mjs';
