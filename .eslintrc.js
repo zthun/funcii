35d7e37568b30084e75db573781d0571ae4270ac
Anthony Bonta <zthun.coding@gmail.com>
2024-03-15 16:10:19 -0500

    build: upgrade typescript and lint-janitor

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,1 @@
-const options = require('@zthun/lint-janitor-config/eslint');
-options.rules['@typescript-eslint/no-namespace'] = 'off';
-options.rules['brace-style'] = 'off';
-module.exports = options;
+module.exports = require('@zthun/lint-janitor-config/eslint');
