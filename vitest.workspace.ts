9c6009ce709901adb3b6248e9ce34a084fb63350
Anthony Bonta <zthun.coding@gmail.com>
2024-04-21 15:06:27 -0500

    refactor: vitest workspaces

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,12 +1,2 @@
 import { defineWorkspace } from 'vitest/config';
-
-export default defineWorkspace([
-  './vitest.config.ts',
-  './packages/helpful-internet/vite.config.ts',
-  './packages/helpful-brands/vite.config.ts',
-  './packages/helpful-query/vite.config.ts',
-  './packages/helpful-fn/vite.config.ts',
-  './packages/helpful-validation/vite.config.ts',
-  './packages/helpful-node/vite.config.ts',
-  './packages/helpful-react/vite.config.ts'
-]);
+export default defineWorkspace(['packages/*/vitest.config.ts']);
